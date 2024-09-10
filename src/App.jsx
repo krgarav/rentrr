b009ca92daef0fee454714c74f6210d8c2cc7d38
krgarav <krgarav@gmail.com>
2024-09-10 15:06:15 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { Routes, Route, Navigate, useNavigate, useLocation } from "react-router-dom";
+import { Dashboard, Auth } from "@/layouts";
+import { SignIn } from "./pages/auth";
+
+function App() {
+
+  const useTokenRedirect = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+ 
+  
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const decoded = jwtDecode(token);
+          if (decoded.Role === "Operator") {
+            if (location.pathname.includes("operator")) {
+              navigate(location.pathname);
+            } else {
+              navigate("/operator/index", { replace: true });
+            }
+          } else if (decoded.Role === "Admin") {
+            if (location.pathname.includes("admin")) {
+              navigate(location.pathname);
+            } else {
+              navigate("/admin/index", { replace: true });
+            }
+          } else if (decoded.Role === "Moderator") {
+            if (location.pathname.includes("moderator")) {
+              navigate(location.pathname);
+            } else {
+              navigate("/moderator/index", { replace: true });
+            }
+          }
+        } catch (error) {
+          console.error("Invalid token:", error);
+          navigate("/auth/login", { replace: true });
+        }
+      } else {
+        // navigate("/", { replace: true });
+        navigate("/auth/login", { replace: true });
+      }
+    }, []);
+  };
+  return (
+    <Routes>
+      <Route path="/*" element={<SignIn />} />
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
