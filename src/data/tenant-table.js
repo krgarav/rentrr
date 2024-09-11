d95c4447a5f8d085448f4647fd05cfa73580c790
krgarav <krgarav@gmail.com>
2024-09-11 15:26:31 +0530

    cursor pointer added

---
+++ src/data/tenant-table.js
@@ -0,0 +1,58 @@
+const tenantTableData = [
+    {
+        img: "/img/team-2.jpeg",
+        name: "John Michael",
+        email: "john@creative-tim.com",
+        members: [
+            { img: "/img/team-2.jpeg", name: "Ryan Tompson" },
+            { img: "/img/team-4.jpeg", name: "Alexander Smith" },
+        ],
+        job: ["Manager", "Organization"],
+        online: true,
+        date: "23/04/18",
+        budget: "₹7,000",
+    },
+    {
+        img: "/img/team-1.jpeg",
+        name: "Alexa Liras",
+        email: "alexa@creative-tim.com",
+        members: [
+            { img: "/img/team-2.jpeg", name: "Ryan Tompson" },
+            { img: "/img/team-4.jpeg", name: "Alexander Smith" },
+        ],
+        job: ["Programator", "Developer"],
+        online: false,
+        date: "11/01/19",
+        budget: "₹6,500",
+    },
+    {
+        img: "/img/team-4.jpeg",
+        name: "Laurent Perrier",
+        email: "laurent@creative-tim.com",
+        members: [
+            { img: "/img/team-2.jpeg", name: "Ryan Tompson" },
+            { img: "/img/team-4.jpeg", name: "Alexander Smith" },
+        ],
+        job: ["Executive", "Projects"],
+        online: true,
+        date: "19/09/17",
+        budget: "₹3,500",
+    },
+    
+    {
+        img: "/img/team-3.jpeg",
+        name: "Michael Levi",
+        email: "michael@creative-tim.com",
+        members: [
+            { img: "/img/team-2.jpeg", name: "Ryan Tompson" },
+            { img: "/img/team-4.jpeg", name: "Alexander Smith" },
+        ],
+        job: ["Programator", "Developer"],
+        online: true,
+        date: "24/12/08",
+        budget: "₹2,200",
+    },
+    
+];
+
+export default tenantTableData;
--- src/pages/dashboard/tables.jsx
+++ src/pages/dashboard/tables.jsx
@@ -10,21 +10,135 @@ import {
 } from "@material-tailwind/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import { authorsTableData, projectsTableData } from "@/data";
-
+import tenantTableData from "@/data/tenant-table";
 export function Tables() {
   return (
     <div className="mt-12 mb-8 flex flex-col gap-12">
       <Card>
         <CardHeader variant="gradient" color="gray" className="mb-8 p-6">
           <Typography variant="h6" color="white">
-            Authors Table
+            Renters Table
           </Typography>
         </CardHeader>
         <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
           <table className="w-full min-w-[640px] table-auto">
             <thead>
               <tr>
-                {["author", "function", "status", "employed", ""].map((el) => (
+                {["Tenant", "Members", "status", "Staying Data", "Amount", ""].map((el) => (
+                  <th
+                    key={el}
+                    className="border-b border-blue-gray-50 py-3 px-5 text-left"
+                  >
+                    <Typography
+                      variant="small"
+                      className="text-[11px] font-bold uppercase text-blue-gray-400"
+                    >
+                      {el}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {tenantTableData.map(
+                ({ img, name, email, job, online, date, members,budget }, key) => {
+                  const className = `py-3 px-5 ${
+                    key === tenantTableData.length - 1
+                      ? ""
+                      : "border-b border-blue-gray-50"
+                  }`;
+
+                  return (
+                    <tr className="hover:bg-gray-100 cursor-pointer" key={name}>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          <Avatar src={img} alt={name} size="sm" variant="rounded" />
+                          <div>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-semibold"
+                            >
+                              {name}
+                            </Typography>
+                            <Typography className="text-xs font-normal text-blue-gray-500">
+                              {email}
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        {members.map(({ img, name }, key) => (
+                          <Tooltip key={name} content={name}>
+                            <Avatar
+                              src={img}
+                              alt={name}
+                              size="xs"
+                              variant="circular"
+                              className={`cursor-pointer border-2 border-white ${
+                                key === 0 ? "" : "-ml-2.5"
+                              }`}
+                            />
+                          </Tooltip>
+                        ))}
+                      </td>
+                      {/* <td className={className}>
+                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                          {job[0]}
+                        </Typography>
+                        <Typography className="text-xs font-normal text-blue-gray-500">
+                          {job[1]}
+                        </Typography>
+                      </td> */}
+                      <td className={className}>
+                        <Chip
+                          variant="gradient"
+                          color={online ? "green" : "blue-gray"}
+                          value={online ? "Paid" : "due"}
+                          className="py-0.5 px-2 text-[11px] font-medium w-fit"
+                        />
+                      </td>
+                      <td className={className}>
+                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                          {date}
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          variant="small"
+                          className="text-xs font-medium text-blue-gray-600"
+                        >
+                          {budget}
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          as="a"
+                          href="#"
+                          className="text-xs font-semibold text-blue-gray-600"
+                        >
+                          Edit
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+      </Card>
+      {/* <Card>
+        <CardHeader variant="gradient" color="gray" className="mb-8 p-6">
+          <Typography variant="h6" color="white">
+            Renters Table
+          </Typography>
+        </CardHeader>
+        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+          <table className="w-full min-w-[640px] table-auto">
+            <thead>
+              <tr>
+                {["Tenant", "function", "status", "employed", ""].map((el) => (
                   <th
                     key={el}
                     className="border-b border-blue-gray-50 py-3 px-5 text-left"
@@ -213,7 +327,7 @@ export function Tables() {
             </tbody>
           </table>
         </CardBody>
-      </Card>
+      </Card> */}
     </div>
   );
 }
